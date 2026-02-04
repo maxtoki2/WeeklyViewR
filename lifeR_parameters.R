f667782ecaef7e9284d4e87274e55e0dffd91e6a
Marchi <mmarchi@indians.com>
2026-02-04 08:28:33 +0100

    parametri e glyphs

--- lifeR_parameters.R
+++ lifeR_parameters.R
@@ -1,12 +1,18 @@
 # collection of all parameters
 
+lista_esclusione <- c("prepartita", "postpartita", "approfondimento", "news", "presentazione", "test")
+
 sky_parameters <- list(
   keywords = tibble::tribble(
     ~Sport, ~kw_include, ~kw_exclude, ~glyph
-    , "Basket", c(" "), c("prepartita", "postpartita", "approfondimento", "news"), "basket.png"
-    , "Motori", " ", c("test"), "motori.png"
-    , "Calcio", c("bologna"), c("prepartita", "postpartita", "approfondimento", "news"), "calcio.png"
-    , "Rugby", c("Six Nations", "Sei Nazioni"), c("prepartita", "postpartita", "approfondimento", "news"), "rugby.png"
+    , "Basket", c(" "), lista_esclusione, "basket.png"
+    , "Motori", " ", lista_esclusione, "motori.png"
+    , "Calcio", c("bologna"), lista_esclusione, "calcio.png"
+    , "Rugby", c("Six Nations", "Sei Nazioni"), lista_esclusione, "rugby.png"
+    , "Atletica", " ", lista_esclusione, "atletica.png"
+    , "Volley", " ", lista_esclusione, "volley.png"
+    , "Altri", " ", c("padel", "judo", lista_esclusione), "sport.png"
+    # , "Tennis", lista_esclusione, "tennis.png"
   )
   , valid_hours = 7:22
 )
--- tv_sports/sandbox/ct_sandbox.R
+++ tv_sports/sandbox/ct_sandbox.R
@@ -1,9 +1,9 @@
 library(rvest)
 library(dplyr)
-library(purrr)
+# library(purrr)
 library(stringr)
 
-url <- "https://www.ceskatelevize.cz/tv-program/27.01.2025/"
+url <- "https://www.ceskatelevize.cz/tv-program/29.01.2026/"
 page <- read_html(url)
 
 # Find all headings, locate the "ČT sport" one
@@ -18,10 +18,21 @@ entries <- siblings %>% html_nodes("li")
 
 # Build tibble with time and title
 sports_df <- tibble(
-  time = entries %>% html_node(".time, strong") %>% html_text(trim = TRUE),
-  title = entries %>% html_node("a, strong, .porad") %>% html_text(trim = TRUE),
-  full = entries %>% html_text(trim = TRUE)
-)
+  time = entries %>% html_node(".progTime") %>% html_text(trim = TRUE),
+  # title = entries %>% html_node("a, strong, .porad") %>% html_text(trim = TRUE),
+  sport = entries %>% html_node(".blockTitle") %>% html_text(trim = TRUE),
+  descr = entries %>% html_node(".progTitle") %>% html_text(trim = TRUE),
+  descr2 = entries %>% html_node("h5") %>% html_text(trim = TRUE),
+  #full = entries %>% html_text(trim = TRUE)
+  live = entries %>%  html_element('span[title="Živě"]') %>% html_text2()
+) %>% 
+  filter(!is.na(sport) & live == "L")
 
-sports_df
+sports_df %>% 
+  mutate(
+    sport = polyglotr::google_translate(sport, "it", "cs")
+    # , descr2 = polyglotr::google_translate(descr2, "it", "cs")
+  )
 # entries[1] %>% html_nodes("*")
+
+# print(schedule_live %>% mutate(titleIT = google_translate(title, "it", "sk")))
