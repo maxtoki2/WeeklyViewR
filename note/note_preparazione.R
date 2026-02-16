165c3ce9b4c9f0156e68cea4c260c336f7868c94
Marchi <mmarchi@indians.com>
2026-02-16 07:43:02 +0100

    fix to match the updated TODOIST API

--- note/note_preparazione.R
+++ note/note_preparazione.R
@@ -5,7 +5,7 @@ TODOIST_TOKEN <- Sys.getenv("TODOIST_API_TOKEN")
 
 # funzione per scaricare le info
 todoist_req <- function(endpoint, query = NULL) {
-  request(paste0("https://api.todoist.com/rest/v2/", endpoint)) |>
+  request(paste0("https://api.todoist.com/api/v1/", endpoint)) |>
     req_headers(
       Authorization = paste("Bearer", TODOIST_TOKEN)
     ) |>
@@ -19,14 +19,14 @@ get_lista_note <- function(
     project_names = c("Casa", "Trasferta", "Acquisti da fare")
     , emojis = emoji(c("house", "airplane", "shopping_cart"))
 ){
-  tasks <- todoist_req("tasks")
+  tasks <- todoist_req("tasks")$results
   
-  projects <- todoist_req("projects")
+  projects <- todoist_req("projects")$results
   
   note <- projects %>% 
     filter(name %in% project_names) %>% 
-    select(id, name, order) %>%
-    rename(project_order = order) %>% 
+    select(id, name, child_order) %>%
+    rename(project_order = child_order) %>% 
     inner_join(tasks, by = c("id" = "project_id")) %>% 
     select(name, content, priority, contains("order"))
   
