d76053c1d33ad589c0695e72cd6af2db46742150
maxtoki2 <max.marchi@gmail.com>
2026-09-17 03:30:02 -0400

    doppia mensa

--- lifeR_parameters.R
+++ lifeR_parameters.R
@@ -11,8 +11,9 @@ param_calendari <- c(
 
 param_rusco_days_offset <- 1
 
+param_mensa_pagine <- c(1, 2)
 param_url_mensa_base <- "https://www.comune.sassomarconi.bologna.it/myportal/C_G972/api/content/download"
-param_url_mensa_doc_id <- "69d6347f34a5a6009a5935f1" # find the new ones at https://www.comune.sassomarconi.bologna.it/servizi/educazione-formazione/servizio-mensa-scolastica
+param_url_mensa_doc_id <- "6aa8fc85ff9f23009973bf24" # find the new ones at https://www.comune.sassomarconi.bologna.it/servizi/educazione-formazione/servizio-mensa-scolastica
 
 param_url_mensa <- glue("{param_url_mensa_base}?id={param_url_mensa_doc_id}")
 
--- mensa/mensa_pulldata.R
+++ mensa/mensa_pulldata.R
@@ -8,9 +8,15 @@ file_mensa <- glue("mensa_{param_url_mensa_doc_id}.RDS")
 if(file_mensa %in% list.files(folder_mensa)){
   mensa_periodo <- readRDS(glue("{folder_mensa}/{file_mensa}"))
 } else {
-  mensa_periodo <- get_menu(param_url_mensa)
+  mensa_periodo <- lapply(1:2, function(x) get_menu(param_url_mensa, pagina = x)) 
   saveRDS(mensa_periodo, glue("{folder_mensa}/{file_mensa}"))
 }
+
+mensa_periodo <- bind_cols(mensa_periodo) %>% 
+  setNames(c("data", "descrizione1", "data2", "descrizione2")) %>% 
+  mutate(descrizione = ifelse(descrizione1 == descrizione2, descrizione1, paste(descrizione1, descrizione2, sep = " | "))) %>% 
+  select(data, descrizione)
+
 mensa <- mensa_periodo %>% 
   filter(data %in% periodo) %>% 
   mutate(gruppo = "mensa") #%>% 
